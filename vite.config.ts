395d43cf1d180986454e9f98c8e57204f5a1b24f
avenged308 <avenged308@gmail.com>
2026-02-25 18:20:20 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import packageJson from "./package.json" with { type: "json" }
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/RTK_training_project/",
 
   server: {
     open: true,
