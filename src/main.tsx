5efca3e93e464602c5777fba8f4760ee77e35118
avenged308 <avenged308@gmail.com>
2026-02-26 11:40:05 +0100

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { Provider } from "react-redux"
-import { App } from "./App"
 import { store } from "./app/store"
 import "./index.css"
+import App from "./App"
 
 const container = document.getElementById("root")
 
